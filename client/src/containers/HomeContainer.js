b0b1198a8a63b59b74bb2c61e05b5620f700b7fa
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 16:00:25 -0400

    added home container

---
+++ client/src/containers/HomeContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;

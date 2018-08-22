bee726c44c340720ca44351c1081ae1255c5d09d
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 15:44:07 -0400

    added volcanoes component

---
+++ client/src/components/volcanoes/VolcanoesByFirstLetter.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class VolcanoesByFirstLetter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map( char => ({[char]: false}) )
+    };
+
+    this.props.checked.forEach( charStr => {
+      const idx = this.state.checked.findIndex(obj => Object.keys(obj)[0] === charStr);
+      this.state.checked[idx][charStr] = true;
+    });
+  }
+
+  handleOnChange(event) {
+    const charStr = event.target.name;
+    const idx     = this.state.checked.findIndex(obj => Object.keys(obj)[0] === charStr);
+    const checked = this.state.checked[idx][charStr];
+    ( checked ? this.props.removeVolcanoes(charStr) : this.props.addVolcanoes(charStr) );
+    this.setState({
+      ...this.state,
+      checked: [...this.state.checked.slice(0, idx), {[charStr]: !checked}, ...this.state.checked.slice(idx+1)],
+    });
+  }
+
+  exposeCheckboxes() {
+    const btn = document.getElementById("selection-button");
+    const arw = document.getElementById("arrow");
+    btn.parentNode.classList.toggle("closed");
+    arw.classList.toggle("down");
+  }
+
+  render() {
+    const charList = this.state.checked.map( (obj, idx) => {
+      let name    = Object.keys(obj)[0];
+      let checked = obj[name];
+      return (
+        <label key={idx}>
+          <input
+            type="checkbox"
+            name={name}
+            checked={checked}
+            onChange={event => this.handleOnChange(event)}
+          />
+          { name }<br />
+        </label>
+      );
+    });
+
+    return (
+      <div className="App-medium">
+
+        <div className="drop-down closed">
+          <div id="selection-button" onClick={() => this.exposeCheckboxes()}>
+            <span>Select volcanoes to view</span>
+            <span className="arrow-container"><i id="arrow" className="down"></i></span>
+          </div>
+          <br /><br />{ charList }
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default VolcanoesByFirstLetter;

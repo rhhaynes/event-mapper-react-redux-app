274e040c21c49c43ec5c1d5422dfe82904aca476
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 14:17:57 -0400

    added root App component

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeContainer from './containers/HomeContainer';
+import EarthquakesContainer from './containers/EarthquakesContainer';
+import HurricanesContainer from './containers/HurricanesContainer';
+import VolcanoesContainer from './containers/VolcanoesContainer';
+import MapContainer from './containers/MapContainer';
+import NavBar from './components/NavBar';
+import './stylesheets/App.css';
+import './stylesheets/dropdown.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+
+          <header className="App-header">
+            <div className="App-container">
+              <NavBar />
+            </div>
+          </header>
+
+          <div className="App-content">
+            <div className="App-container">
+              <Route exact path="/" component={HomeContainer} />
+              <Route exact path="/earthquakes" component={EarthquakesContainer} />
+              <Route exact path="/hurricanes" component={HurricanesContainer} />
+              <Route exact path="/volcanoes" component={VolcanoesContainer} />
+              <MapContainer />
+            </div>
+          </div>
+
+          <footer className="App-footer">
+            <div className="App-container">
+              <div className="App-small">
+                &copy; 2018 EventMapper
+              </div>
+            </div>
+          </footer>
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;

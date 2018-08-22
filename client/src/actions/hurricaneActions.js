48007ae98b7d6c3910e0382337ec6920b2f14d02
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 14:39:44 -0400

    added hurricane actions

---
+++ client/src/actions/hurricaneActions.js
@@ -0,0 +1,36 @@
+export function addHurricanes(yearStr) {
+  return dispatch => (
+    fetch(`/api/hurricanes/${yearStr}`)
+    .then(resp => resp.json())
+    .then(respJson => {
+
+      const hurricanes = {
+        [yearStr]: respJson.map( feature => ({
+          [feature.name]: {
+            status: feature.status,
+            category: feature.category,
+            deaths: feature.deaths,
+            latlng: feature.geolocations
+          }
+        }))
+      };
+
+      dispatch({type: 'ADD_HURRICANES', payload: hurricanes});
+    })
+  );
+}
+
+export function removeHurricanes(yearStr) {
+  return {
+    type: 'REMOVE_HURRICANES',
+    year: yearStr
+  };
+}
+
+export function toggleHurricanes(yearStr, nameStr) {
+  return {
+    type: 'TOGGLE_HURRICANES',
+    year: yearStr,
+    name: nameStr
+  };
+}

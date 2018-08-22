23bac43c93785ba0a65091fff6e4d78f01cd6f5a
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 15:33:10 -0400

    added components to render hurricanes by name

---
+++ client/src/components/hurricanes/Hurricane.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Hurricane extends Component {
+  render() {
+    return (
+      <label>
+        &nbsp;&nbsp;
+        <input
+          type="checkbox"
+          checked={this.props.checked}
+          onChange={event => this.props.toggleHurricanes(this.props.year, this.props.name)}
+        />
+        { this.props.name }
+      </label>
+    );
+  }
+}
+
+export default Hurricane;
---
+++ client/src/components/hurricanes/HurricanesByName.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Hurricane from './Hurricane';
+
+class HurricanesByName extends Component {
+  render() {
+    const nameList = (
+      ( !Array.isArray(this.props.hurricanes) || !this.props.hurricanes.length )
+      ? null
+      : this.props.hurricanes.map( obj => {
+        let yearStr = Object.keys(obj)[0];
+        let hurrArr = obj[yearStr];
+        return (
+          <div key={yearStr} className="App-small-sub">
+            <span style={{fontWeight:'bold'}}>
+              { yearStr }:
+            </span>
+            { hurrArr.map( hurr => {
+              let nameStr = Object.keys(hurr)[0];
+              let checked = hurr[nameStr].status;
+              return (
+                <Hurricane
+                  key={yearStr+nameStr}
+                  year={yearStr}
+                  name={nameStr}
+                  checked={checked}
+                  toggleHurricanes={this.props.toggleHurricanes}
+                />
+              );
+            })}
+          </div>
+        );
+      })
+    );
+
+    return (
+      <div>
+        { nameList }
+      </div>
+    );
+  }
+}
+
+export default HurricanesByName;

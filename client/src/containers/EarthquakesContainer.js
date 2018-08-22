dc653487f3cffa3d7bf38ff75d31fe64d575684b
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 15:04:00 -0400

    added earthquakes container

---
+++ client/src/containers/EarthquakesContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addEarthquakes, removeEarthquakes } from '../actions/earthquakeActions';
+import EarthquakesByDate from '../components/earthquakes/EarthquakesByDate';
+
+class EarthquakesContainer extends Component {
+  render() {
+    return (
+      <div>
+
+        <EarthquakesByDate
+          checked={this.props.earthquakes.map(obj => Object.keys(obj)[0])}
+          addEarthquakes={this.props.addEarthquakes}
+          removeEarthquakes={this.props.removeEarthquakes}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ earthquakes: state.earthquakes }),
+  { addEarthquakes, removeEarthquakes }
+)(EarthquakesContainer);

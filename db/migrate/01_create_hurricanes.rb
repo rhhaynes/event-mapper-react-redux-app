9e352aa75fdec09474ea2b1fe17d88db20972cee
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 10:46:29 -0400

    added attributes to hurricanes table

---
+++ db/migrate/01_create_hurricanes.rb
@@ -0,0 +1,11 @@
+class CreateHurricanes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :hurricanes do |t|
+      t.string  :year
+      t.string  :name
+      t.boolean :status
+      t.integer :category
+      t.integer :deaths
+    end
+  end
+end

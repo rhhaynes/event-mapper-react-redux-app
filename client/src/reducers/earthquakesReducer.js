370c1c693d718d3418e05e51f67296d3dbc30f82
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 14:49:46 -0400

    added earthquakes reducer

---
+++ client/src/reducers/earthquakesReducer.js
@@ -0,0 +1,18 @@
+export default (state = [], action) => {
+
+  switch (action.type) {
+
+    case 'ADD_EARTHQUAKES': {
+      return [...state, action.payload];
+    }
+
+    case 'REMOVE_EARTHQUAKES': {
+      const idx = state.findIndex(obj => Object.keys(obj)[0] === action.query);
+      return [...state.slice(0, idx), ...state.slice(idx+1)];
+    }
+
+    default: {
+      return state;
+    }
+  }
+}

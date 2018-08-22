c76c3114f3753e3e87aa1eb470420d4e1b393025
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 15:58:50 -0400

    added map component for volcano markers

---
+++ client/src/components/map/VolcanoMarker.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Marker, InfoWindow } from 'react-google-maps';
+
+class VolcanoMarker extends Component {
+  constructor() {
+    super();
+    this.state = { showInfoWindow: false };
+  }
+
+  toggleInfoWindow() {
+    this.setState({ showInfoWindow: !this.state.showInfoWindow });
+  }
+
+  render() {
+    return (
+      <Marker
+        position={this.props.latlng}
+        icon={{
+          path: 'm 0,-6 l -5.2,9 l 10.4,0 z',
+          fillColor: '#f00',
+          fillOpacity: 1,
+          strokeColor: '#f00',
+          strokeOpacity: 1
+        }}
+        onMouseOver={() => this.toggleInfoWindow()}
+        onMouseOut={() => this.toggleInfoWindow()}
+      >
+        { this.state.showInfoWindow &&
+          <InfoWindow>
+            <div className="App-info-window">
+              <span style={{fontWeight:'bold'}}>{this.props.name}</span><br />
+              {this.props.location}<br />
+              {this.props.type}<br />
+              {`Elevation of ${this.props.elev_m} m`}
+            </div>
+          </InfoWindow>
+        }
+      </Marker>
+    );
+  }
+}
+
+export default VolcanoMarker;

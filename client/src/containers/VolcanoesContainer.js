66e780464e7d0c68ad89b29b49f509290aa580ad
R. Harris Haynes <harrishaynes@gmail.com>
2018-08-22 15:43:47 -0400

    added volcanoes container

---
+++ client/src/containers/VolcanoesContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addVolcanoes, removeVolcanoes } from '../actions/volcanoActions';
+import VolcanoesByFirstLetter from '../components/volcanoes/VolcanoesByFirstLetter';
+
+class VolcanoesContainer extends Component {
+  render() {
+    return (
+      <div>
+
+        <VolcanoesByFirstLetter
+          checked={this.props.volcanoes.map(obj => Object.keys(obj)[0])}
+          addVolcanoes={this.props.addVolcanoes}
+          removeVolcanoes={this.props.removeVolcanoes}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ volcanoes: state.volcanoes }),
+  { addVolcanoes, removeVolcanoes }
+)(VolcanoesContainer);

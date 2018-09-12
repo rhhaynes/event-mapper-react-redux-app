4494586550a441660aabba5f122c4a171e3c7199
R. Harris Haynes <harrishaynes@gmail.com>
2018-09-12 16:16:12 -0400

    added locations (home) container

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ClearMapContainer from './ClearMapContainer';
+import { fetchLocations, toggleLocations } from '../actions/locationActions';
+import LocationsByName from '../components/locations/LocationsByName';
 
 class HomeContainer extends Component {
+  componentDidMount() {
+    if ( !Array.isArray(this.props.locations) || !this.props.locations.length ) {
+      this.props.fetchLocations();
+    }
+  }
+
   render() {
     return (
       <div>
+
+        <LocationsByName
+          locations={this.props.locations}
+          toggleLocations={this.props.toggleLocations}
+        />
+        <ClearMapContainer />
+
       </div>
     );
   }
 }
 
-export default HomeContainer;
+export default connect(
+  state => ({ locations: state.locations }),
+  { fetchLocations, toggleLocations }
+)(HomeContainer);

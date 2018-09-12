77beeb84366ba2e0ffe324d692381970554009c6
R. Harris Haynes <harrishaynes@gmail.com>
2018-09-12 15:48:37 -0400

    added api routes for locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope '/api' do
-    get '/hurricanes/:yr' => 'hurricanes#show'
-    get '/volcanoes/:ltr' => 'volcanos#show'
+    get  '/locations' => 'locations#index'
+    post '/locations' => 'locations#create'
+    get  '/hurricanes/:yr' => 'hurricanes#show'
+    get  '/volcanoes/:ltr' => 'volcanos#show'
   end
 end

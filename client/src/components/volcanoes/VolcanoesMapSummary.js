cf85fa2de5d2c34b56d482a08e5225a39126b601
R. Harris Haynes <harrishaynes@gmail.com>
2018-09-21 22:46:48 -0400

    created VolcanoesMapSummary component

---
+++ client/src/components/volcanoes/VolcanoesMapSummary.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class VolcanoesMapSummary extends Component {
+  render() {
+    const nameList = (
+      ( !Array.isArray(this.props.volcanoes) || !this.props.volcanoes.length )
+      ? null
+      : this.props.volcanoes.map( obj => {
+        let charStr = Object.keys(obj)[0];
+        let volArr = obj[charStr];
+        return (
+          <div key={charStr} className="App-content-small">
+            <span style={{fontWeight:'bold'}}>{ charStr }:&nbsp;&nbsp;</span>
+            { volArr.map(vol => Object.keys(vol)[0]).join('; ')}
+          </div>
+        );
+      })
+    );
+
+    return (
+      <div>
+        { !!nameList && <div className="App-summary-subtitle">Volcanoes</div> }
+        { nameList }
+      </div>
+    );
+  }
+}
+
+export default VolcanoesMapSummary;

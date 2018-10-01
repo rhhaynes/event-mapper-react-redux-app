fab42c973ef6e2a4f69874faa8391661b897aaff
R. Harris Haynes <harrishaynes@gmail.com>
2018-10-01 10:28:08 -0400

    combined maps reducer with others

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import mapOptions from './mapsReducer';
 import earthquakes from './earthquakesReducer';
 import hurricanes from './hurricanesReducer';
 import locations from './locationsReducer';
 import volcanoes from './volcanoesReducer';
 
 export default combineReducers({
+  mapOptions,
   earthquakes,
   hurricanes,
   locations,

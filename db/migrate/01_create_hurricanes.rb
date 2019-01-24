62929732904813969a2125bf0ef5f65bbdebebc9
R. Harris Haynes <harrishaynes@gmail.com>
2019-01-23 22:33:50 -0500

    added region attribute to hurricanes table

--- db/migrate/01_create_hurricanes.rb
+++ db/migrate/01_create_hurricanes.rb
@@ -2,6 +2,7 @@ class CreateHurricanes < ActiveRecord::Migration[5.1]
   def change
     create_table :hurricanes do |t|
       t.string  :year
+      t.string  :region
       t.string  :name
       t.boolean :status
       t.integer :category
